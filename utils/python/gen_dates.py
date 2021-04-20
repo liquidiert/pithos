7c8ea17748172cdf7609213c3fd3378c79e3dcec
liquidiert <36642878+liquidiert@users.noreply.github.com>
2021-04-21 00:27:29 +0200

    formatted

--- utils/python/gen_dates.py
+++ utils/python/gen_dates.py
@@ -6,7 +6,8 @@ locale.setlocale(locale.LC_TIME, "de_DE.utf8")
 start_date = datetime.datetime.now()
 number_of_days = int(input("How many days shall be generated? "))
 
-date_list = [(start_date + datetime.timedelta(days = day)).strftime("%a. %d.%m.%Y, %d.%m.%Y") for day in range(number_of_days)]
+date_list = [(start_date + datetime.timedelta(days=day)
+              ).strftime("%a. %d.%m.%Y, %d.%m.%Y") for day in range(number_of_days)]
 
 print()
 for date in date_list:
