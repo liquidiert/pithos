b14cbe45f6faf3c3790eecf5fe9e216373d8da73
liquidiert <36642878+liquidiert@users.noreply.github.com>
2021-04-21 00:27:00 +0200

    Create gen_dates.py

---
+++ utils/python/gen_dates.py
@@ -0,0 +1,14 @@
+import datetime
+import locale
+
+locale.setlocale(locale.LC_TIME, "de_DE.utf8")
+
+start_date = datetime.datetime.now()
+number_of_days = int(input("How many days shall be generated? "))
+
+date_list = [(start_date + datetime.timedelta(days = day)).strftime("%a. %d.%m.%Y, %d.%m.%Y") for day in range(number_of_days)]
+
+print()
+for date in date_list:
+    print(date)
+print("\nREMEMBER TO CHECK FOR DAYS OF / SUNDAYS")
